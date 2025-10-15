4cf402b4fd4dd4e53a53f15107571522c3ab7272
ellawendy <ellawendy13@gmail.com>
2025-10-15 20:26:27 +0300

    add alx-react-app-props

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Alice", age: 25, bio: "Loves hiking and photography" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <div>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <UserProfile />
+        <Footer />
+      </div>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
