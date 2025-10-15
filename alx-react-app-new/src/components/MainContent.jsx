5eceea2cd5fc865ec1107eaf5d41cec19390a041
ellawendy <ellawendy13@gmail.com>
2025-10-15 20:07:45 +0300

    add alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f9f9f9' }}>
+      <p>This is the main content of the app.</p>
+    </main>
+  );
+}
+
+export default MainContent;
