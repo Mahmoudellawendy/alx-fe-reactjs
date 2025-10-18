4e99c766395f9bead6a7b91fc5477ca5c46abb80
ellawendy <ellawendy13@gmail.com>
2025-10-18 18:13:03 +0300

    Add Advanced React Routerwith nested, dynamic, and protected routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,12 +3,12 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
+  Navigate
 } from 'react-router-dom';
 
 import Home from './components/Home';
 import Profile from './components/Profile';
-import Post from './components/Post';
+import BlogPost from './components/BlogPost';
 import Login from './components/Login';
 import NotFound from './components/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -17,26 +17,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Public Route */}
         <Route path="/" element={<Home />} />
-
-        {/* Protected Route with Nested Routes */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Dynamic Route */}
-        <Route path="/post/:postId" element={<Post />} />
-
-        {/* Login Route */}
+        <Route path="/profile/*" element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        } />
+        <Route path="/blog/:id" element={<BlogPost />} />
         <Route path="/login" element={<Login />} />
-
-        {/* 404 Not Found */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
